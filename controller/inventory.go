ae12d8cc1f928f46a0372bb56efc742e1bd48240
Sebastian Pinto <sebastian.pinto.g@usach.cl>
2021-11-11 00:53:36 -0300

    controlador inicial

---
+++ controller/inventory.go
@@ -0,0 +1,18 @@
+package controlador
+
+import (
+	jwt "github.com/appleboy/gin-jwt/v2"
+	"github.com/gin-gonic/gin"
+)
+
+type InventoryController struct {
+}
+
+func (inventoryController *InventoryController) outes(base *gin.RouterGroup, authNormal *jwt.GinJWTMiddleware) *gin.RouterGroup {
+
+	inventoryRouter := base.Group("/inventories")
+	{
+		inventoryRouter.GET("")
+	}
+
+}
